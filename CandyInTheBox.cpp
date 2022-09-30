7b5856629079be17a9a104840dc3220692a9d9ea
Sedanur Savaş <44877350+sedsax@users.noreply.github.com>
2022-09-30 09:17:43 +0300

    Create CandyInTheBox.cpp

---
+++ CandyInTheBox.cpp
@@ -0,0 +1,7 @@
+/*
+Bu problemi dün akşam okudum (29.09.2022 - 23.20). Bir şeyler anlamıştım ama yanlış anlamışım. Bu sabah (8.30 civarı) adım adım tekrar inceledim ve evet, anladım! 
+Inputların ne olduğunu, algoritmanın nasıl çalıştığını, output u neye göre verdiğini anladım. Geriye anladığım algoritmayı koda dökmek kaldı. 
+
+Problem = Candy in the Box
+https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/candy-in-the-box-75b63839/
+*/
